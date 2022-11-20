f560391253cc25a3da3b8893369aefd57f368c6e
danielSafonyan <danielsafonyan@gmail.com>
2022-11-20 17:48:49 +0000

    Added Payment Modal

--- script.js
+++ script.js
@@ -2,13 +2,30 @@ import menuArray from './data.js'
 
 const userMenuArray = menuArray.map(el => ({...el, inCart: 0}))
 
+const form = document.querySelector('form')
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const userData = new FormData(form);
+    const userName = userData.get('fullName').split(" ").shift();
+    const thankYouHTML = `
+    <div class="thank-you-message">Thanks, ${userName}! Your order is on its way!</div>
+    `
+    document.getElementById("payment-details").innerHTML = thankYouHTML
+})
+
 document.addEventListener('click', handleClick)
 
 function handleClick(event) {
     const dataset = event.target.dataset;
     if (dataset.addProductid) {
         const addProduct = userMenuArray.find(el => el.id == dataset.addProductid)
-        addProduct.inCart++
+        if (addProduct.inCart > 9) {
+            console.log("10 is maximum.")
+            return
+        } else {
+            addProduct.inCart++
+        }
+        
     }
     if (dataset.subProductid) {
         const subProduct = userMenuArray.find(el => el.id == dataset.subProductid)
@@ -18,16 +35,42 @@ function handleClick(event) {
             console.log("Cant go below 0.")
             return
         }
-        
     }
+
+    if (dataset.removeCartItemid) {
+        userMenuArray.find(el => el.id == dataset.removeCartItemid).inCart = 0
+    }
+
+    if (dataset.checkoutButton) {
+        if (dataset.checkoutButton === 'true') {
+            document.getElementById('payment-details').style.display = "flex"
+        } else {
+            console.log("Can't checkout!")
+        }
+    }
+
     renderMenu()
     renderCheckout()
 }
+
+// function renderPaymentDetails() {
+//         TODO
+// }
 
 function renderCheckout() {
     const itemsInCart = userMenuArray.filter(el => el.inCart > 0)
     renderCartItems(itemsInCart)
     renderTotalPrice(itemsInCart)
+    renderCheckoutBtn(itemsInCart.length)
+}
+
+function renderCheckoutBtn(itemsInCartCount) {
+    const isActive = Boolean(itemsInCartCount) 
+    const isDisabledClass = isActive ? "" : "disabled" 
+    
+    const checkoutBtn = (`<button class="checkout-button ${isDisabledClass}" data-checkout-button=${isActive}>Complete Order</button>`)
+
+    document.getElementById('checkout-btn-container').innerHTML = checkoutBtn
 }
 
 function renderTotalPrice(itemsInCartArray) {
@@ -65,7 +108,9 @@ function renderMenu() {
 
 function renderMenuItem(menuItemObject) {
     const subIsDisabled = menuItemObject.inCart === 0
+    const addIsDisabled = menuItemObject.inCart === 10
     const subStyle = subIsDisabled ? "disabled" : ""
+    const addStyle = addIsDisabled ? "disabled" : ""
     return (
         `
             <div class="menu-item">
@@ -80,7 +125,7 @@ function renderMenuItem(menuItemObject) {
                 <div class="item-quantity">
                     <div class="item-btn ${subStyle}" data-sub-productID=${menuItemObject.id}>-</div>
                     <div class="item-num">${menuItemObject.inCart}</div>
-                    <div class="item-btn" data-add-productID=
+                    <div class="item-btn ${addStyle}" data-add-productID=
                     ${menuItemObject.id}>+</div>
                 </div>
             </div>
@@ -89,23 +134,4 @@ function renderMenuItem(menuItemObject) {
 }
 
 renderMenu()
-
-
-// emoji
-// : 
-// "🍕"
-// id
-// : 
-// 0
-// inCart
-// : 
-// 0
-// ingredients
-// : 
-// (3) ['pepperoni', 'mushrom', 'mozarella']
-// name
-// : 
-// "Pizza"
-// price
-// : 
-// 14
+renderCheckout()
